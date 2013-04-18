e06b1b611c9db07e1c07077d7425964448f1f2de
jacklibj <worinco@163.com>
2013-04-18 23:56:54 +0800

    a few add

--- qcad/rs_line.h
+++ qcad/rs_line.h
@@ -71,5 +71,73 @@ public:
 	virtual RS_Vector getStartpoint() const {
 		return data.startpoint;
 	}
+	/** @return the endpoint of the entiry */
+	virtual RS_Vector getEndpoint() const {
+		return data.endpoint;
+	}
+	/** Sets the startpoint */
+	void setStartpoint(RS_Vector s) {
+		data.startpoint = s;
+		caculateBorders();
+	}
+	/** Sets the endpoint */
+	void setEndpoint(RS_Vector e) {
+		data.endpoint = e;
+        calculateBorders();
+	}
+	/** 
+	* @return Direction 1. The angle at which the line starts at
+	* the startpoint.
+	*/
+	double getDirection1() const {
+		return getAngle1();
+	}
+	/** 
+	* @return Direction 2. The angle at which the line starts at
+	* the endpoint.
+	*/
+	double getDirection2() const {
+		return getAngle2();
+	}
+	virtual void moveStartpoint(const RS_Vector& pos);
+	virtual void moveEndpoint(const RS_Vector& pos);
+	virtual RS2::Ending getTrimPoint(const RS_Vector& coord, 
+		const RS_Vector& trimPoint);
+	virtual void reverse();
+	RS_Vector getMiddlepoint() {
+		return (data.startpoint + data.endpoint)/2.0;
+	}
+	/** Sets the y coordinate of the startpoint */
+	void setStartpointY(double val) {
+		data.startpoint.y = val;
+		calculateBorders();
+	}
+	/** Sets the y coordinate of the endpoint */
+	void setEndpointY(double val) {
+		data.endpoint.y = val;
+		calculateBorders();
+	}
+	virtual bool hasEndpointWithinWindow(RS_Vector v1, RS_Vector v2);
 
+	/** 
+	* @return The length of the line.
+	*/
+	virtual double getLength() {
+		return data.startpoint.distanceTo(data.endpoint);
+	}
+
+	/** 
+	* @return The angle of the line (from start to endpoint).
+	*/
+	virtual double getAngle1() const {
+		return data.startpoint.angleTo(data.endpoint);
+	}
+	/** 
+	* @return The angle of the line (from end to startpoint).
+	*/
+	virtual double getAngle2() const {
+		return data.endpoint.angleTo(data.startpoint);
+	}
+protected:
+	RS_LineData data;
 };
