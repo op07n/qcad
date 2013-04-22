2427a5c1c08a7a16bbdbbca11af31b5ea6ea6588
jacklibj <worinco@163.com>
2013-04-22 22:06:43 +0800

    two file finish

--- qcad/rs_undocycle.h
+++ qcad/rs_undocycle.h
@@ -49,8 +49,36 @@ public:
 	* Iteration through undoable elements in this item.
 	*/
 	RS_Undoable* getFirstUndoable() {
-		return undoables.remove(u);
+		return undoables.first();
 	}
+
+	/**
+	* Iteration through undoable elements in this item.
+	*/
+
+	RS_Undoable* getNextUndoable() {
+		return undoables.next();
+	}
+
+	friend std::ostream& operator << (std::ostream& os,
+		                              RS_UndoCycle& i) {
+			os << " Undo item: " << "\n";
+			os << " Undoable ids: ";
+			for (RS_Undoable* u= i.getFirstUndoable();
+				u!=NULL; u=i.getNextUndoable()) {
+
+					if (u->undoRtti()==RS2::UndoableEntity) 
+					{
+						RS_Entity* e = (RS_Entity*)u;
+						os<<e->getId() << (u->isUndone() ? "*" : "") << " ";
+					} else {
+						os << "|";
+					}
+			}
+			return os;
+	}
+	friend class RS_Undo;
+
 
 private:
 	//
