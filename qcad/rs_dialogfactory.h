08a7b189178fa8106f8b49e97ba57a0a046cc001
jacklibj <worinco@163.com>
2013-04-24 22:12:42 +0800

    some dialog realted update

--- qcad/rs_dialogfactory.h
+++ qcad/rs_dialogfactory.h
@@ -3,6 +3,34 @@
 
 #include "rs_dialogfactoryinterface.h"
 #include "rs_dialogfactoryadapter.h"
+#include "rs_debug.h"
+
+#define RS_DIALOGFACTORY RS_DialogFactory::instance()->getFactoryObject()
+
+/** 
+* Interface for objects that can create and show dialogs.
+*/
+class RS_DialogFactory {
+
+private:
+	RS_DialogFactory();
+
+public:
+	virtual ~RS_DialogFactory() {}
+
+	static RS_DialogFactory* instance();
+
+	void setFactoryObject(RS_DialogFactoryInterface* fo);
+	RS_DialogFactoryInterface* getFactoryObject();
+
+	void commandMessage(const RS_String& m);
+
+private:
+	RS_DialogFactoryInterface* factoryObject;
+	RS_DialogFactoryAdapter factoryAdapter;
+	static RS_DialogFactory* uniqueInstance;
+
+};
 
 
 #endif
