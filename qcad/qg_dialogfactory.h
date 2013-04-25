72842819a5e5b96cdfafcc2c0fe564e0976bf8d7
jacklibj <worinco@163.com>
2013-04-25 13:13:19 +0800

    little update

--- qcad/qg_dialogfactory.h
+++ qcad/qg_dialogfactory.h
@@ -47,6 +47,192 @@ public:
 		coordinateWidget = cw;
 	}
 
+	/** 
+	* Links this dialog factory to a mouse widget.
+	*/
+	virtual void setMouseWidget(QG_MouseWidget* mw) {
+		mouseWidget = mw;
+	}
+
+	/** 
+	* Link this dialog factory to selection widget
+	*/
+	virtual void setSelctionWidget(QG_SelectionWidget* sw) {
+		selectionWidget = sw;
+	}
+
+	/** 
+	* Link this dialog factory to the cad tool bar.
+	*/
+	virtual void setCadToolBar(QG_CadToolBar* ctb) {
+		cadToolBar = ctb;
+	}
+
+
+	/** 
+	* @return cad tool bar or NULL
+	*/
+	QG_CadToolBar* getCadToolBar() {
+		return cadToolBar;
+	}
+
+	/** 
+	* Links the dialog facotory to a command widget.
+	*/
+	virtual void setCommandWidget(QG_CommandWidget* cw) {
+		commandWidget = cw;
+	}
+
+	/** 
+	* @return command widget pr NULL
+	*/
+	QG_CommandWidget* getCommandWidget() {
+		return commandWidget;
+	}
+
+	/** 
+	* Links the dialog factory to a main app window
+	*/
+	virtual void setMainWindow(QG_MainWindowInterface* mw) {
+		mainWindow = mw;
+	}
+
+
+	virtual void requestWarningDialog(const RS_String& warning);
+
+	virtual RS_GraphicView* requestNewDocument(const RS_String& filename  = RS_String::null , RS_Document* doc =NULL );
+	virtual void requestSimulationControls();
+
+	virtual RS_Layer* requestNewLayerDialog(
+		RS_LayerList* layerList  = NULL );
+	virtual RS_Layer* requestLayerRemovalDialog( 
+		RS_LayerList* layerList  = NULL);
+	virtual RS_Layer* requestEditLayerDialog(
+		RS_LayerList* layerList = NULL );
+
+	virtual RS_BlockData requestNewBlockDialog(RS_BlockList* blockList);
+	virtual RS_Block* requestBlockRemovalDialog(
+		RS_BlockList* blockList);
+	virtual RS_BlockData requestBlockAtributesDialog(
+		RS_BlockList* blockList);
+	virtual void requestEditBlockWindow(
+		RS_BlockList* /*blockList*/) {}
+	virtual void closeEditBlockWindow(
+		RS_Block* ) {}
+	//
+    //
+
+	virtual RS_String requestImageOpenDialog();
+
+
+	virtual void requestOptions(RS_ActionInterface* action,
+		                        bool on, bool update = false);
+
+protected:
+	virtual void requestPrintPreviewOptions(RS_ActionInterface* action,
+		                                    bool on, bool update);
+	virtual void requestLineOptions(RS_ActionInterface* action,
+		                            bool on);
+	virtual void requestPolyLineOptions(RS_ActionInterface* action,
+		                                bool on, bool update);
+	virtual void requestLineAngleOptions(RS_ActionInterface* action,
+		                                 bool on, bool update);
+	virtual void requestLineRelAngleOptions(RS_ActionInterface* action,
+		bool on, bool update);
+	virtual void requestLineParallelOptions(RS_ActionInterface* action,
+		bool on, bool update);
+	virtual void requestLineParallelThroughOptions(RS_ActionInterface* action,
+		bool on, bool update);
+	virtual void requestLineBisectorOptions(RS_ActionInterface* action,
+		bool on, bool update);
+	virtual void requestLinePolygonOptions(RS_ActionInterface* action,
+		bool on, bool update);
+	virtual void requestLinePolygon2Options(RS_ActionInterface* action,
+		bool on, bool update);
+
+	virtual void requestArcOptions(RS_ActionInterface* action,
+		bool on, bool update);
+
+	virtual void requestArcTangentialOptions(RS_ActionInterface* action,
+		bool on, bool update);
+
+	virtual void requestCircleOptions(RS_ActionInterface* action,
+		bool on, bool update);
+
+	virtual void requestSplineOptions(RS_ActionInterface* action,
+		bool on, bool update);
+
+	virtual void requestTextOptions(RS_ActionInterface* action,
+		bool on, bool update);
+
+	virtual void requestDimensionOptions(RS_ActionInterface* action,
+		bool on, bool update);
+
+	virtual void requestDimLinearOptions(RS_ActionInterface* action,
+		bool on, bool update);
+
+	virtual void requestInsertOptions(RS_ActionInterface* action,
+		bool on, bool update);
+	virtual void requestImageOptions(RS_ActionInterface* action,
+		bool on, bool update);
+
+	virtual void requestTrimAmountOptions(RS_ActionInterface* action,
+		bool on, bool update);
+
+	virtual void requestMoveRotateOptions(RS_ActionInterface* action,
+		bool on, bool update);
+
+	virtual void requestBevelOptions(RS_ActionInterface* action,
+		bool on, bool update);
+
+	virtual void requestRoundoptions(RS_ActionInterface* action,
+		bool on, bool update);
+
+	virtual void requestLibraryInsertOptions(RS_ActionInterface* action,
+		bool on, bool update);
+
+public:
+		virtual void requestSnapDistOptions(double& dist, bool on);
+
+public:
+	virtual void requestToolBar(RS2::ToolBarId id);
+	virtual void requestToolBarSelect(RS_ActionInterface* selectAction, 
+		                               RS2::ActionType nextAction);
+
+	virtual bool requestAttributesDialog(RS_AttributesData& data, 
+		RS_LayerList& layerList);
+
+	virtual bool requestMoveDialog(RS_MoveData& data);
+	virtual bool requestRotateDialog(RS_RotateData& data);
+	virtual bool requestScaleDialog(RS_ScaleData& data);
+	virtual bool requestMirrorDialog(RS_MirrorData& data);
+	virtual bool requestMoveRotateDialog(RS_MoveRotateData& data);
+	virtual bool requestRotate2Dialog(RS_Rotate2Data& data);
+
+	virtual bool requestModifyEntityDialog(RS_Entity* entity);
+	virtual bool requestTextDialog(RS_Text* text);
+	virtual bool requestHatchDialog(RS_Hatch* hatch);
+
+#ifdef RS_CAM
+	virtual bool requestCamOptionsDialog(RS_Graphic& graphic);
+#endif
+
+	virtual bool requestOptionsGeneralDialog();
+	virtual bool requestOptionsDrawingDialog(RS_Graphic& graphic);
+
+	virtual void requestPreviousMenu();
+
+	virtual void updateCoordinateWidget(const RS_Vector& abs, 
+		const RS_Vector& rel, 
+		bool updateFormat =false);
+
+	virtual void updateMouseWidget(const RS_String& left, 
+		const RS_String& right);
+
+	virtual void commandMessage(const RS_String& message);
+	virtual bool isAdapter() { return false; }
+
+	static QString extToFormat(const QString& ext);
 
 protected:
 	//! Pointer to the widget which can host dialogs
