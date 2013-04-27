a46fad8bae0c1718401ec3fa54fa318f5085f975
jacklibj <worinco@163.com>
2013-04-27 21:56:07 +0800

    few update

--- qcad/rs_dimaligned.h
+++ qcad/rs_dimaligned.h
@@ -28,11 +28,50 @@ public:
 						  this->extensionPoint2 = extensionPoint2;
 	}
 
+	friend class RS_DimAligned;
+	friend class RS_ActionDimAligned;
+
+	friend std::ostream& operator << (std::ostream& os,
+		                              const RS_DimAlignedData& dd)  {
+	   os << "(" << dd.extensionPoint1 << "/"<< dd.extensionPoint2 <<")";
+	   return os;
+	}
+
 private:
 	/** definition point, Startpoint of the first extension line, */
 	RS_Vector extensionPoint1;
 	/** definition point, Startpoint of the second extension line, */
 	RS_Vector extensionPoint2;
+};
+
+
+class RS_DimAligned : public RS_Dimension {
+public:
+	RS_DimAligned(RS_EntityContainer* parent,
+		          const RS_DimensionData& d,
+				  const RS_DimAlignedData& ed);
+	virtual ~RS_DimAligned() {}
+
+	virtual RS_Entity* clone() {
+		RS_DimAligned* d = new RS_DimAligned(*this);
+		d->entities.setAutoDelete(entities.autoDelete());
+		d->initId();
+		d->detach();
+		return d;
+	}
+
+	/** 
+	* @ return RS2::entityDimAligned */
+	virtual RS2::EntityType rtti() const {
+		return RS2::EntityDimAligned;
+	}
+
+
+	/** 
+	* @return 
+protected:
+	/** Extended data. */
+	RS_DimAlignedData edata;
 };
 
 #endif
