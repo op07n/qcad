9c8f4d0104ef6cd32ad7d299606f6b8881a04d15
jacklibj <worinco@163.com>
2013-04-29 00:30:16 +0800

    little update
    8307/5208

--- qcad/rs_spline.h
+++ qcad/rs_spline.h
@@ -60,11 +60,91 @@ public:
 		return false;
 	}
 
-	/** @return false */
-	virtual bool isEdge() const {
-		return false;
+	/** @return Copy of data that defines the spline */
+	RS_SplineData  getData() const {
+		return data;
 	}
 
-	/** @return Copy of data that defines the spline */
+	/** Sets the splines degree (1-3) */
+	void setDegree(int deg) {
+		if(deg>=1 && deg<=3) {
+			data.degree = deg;
+		}
 
+	}
+
+	/** @return Degree of this spline curve (1-3) */
+	int getDegree() {
+		return data.degree;
+	}
+
+	/** @return 0. */
+	int getNumberOfKnots() {
+		return data.controlPoints.count();
+	}
+
+	/** 
+	* @return true if the spline is closed.
+	* @return false otherwise.
+	*/
+	bool isClosed() {
+		return data.closed;
+	}
+
+	/** 
+	* Sets the closed flag ot this spline
+	*/
+	void setClosed(bool c) {
+		data.closed = c;
+	}
+	virtual RS_VectorSolutions getRefPoints();
+	virtual RS_Vector getNearestRef(const RS_Vector& coord, 
+		double* dist  = NULL );
+	virtual RS_Vector getNearestSelectedRef(const RS_Vector& coord,
+		                           double* dist = NULL);
+
+	//
+	//
+	//
+	//
+
+	void update();
+
+
+	virtual RS_Vector getNearestEndPoint(const RS_Vector& coord, 
+		                                 double* dist = NULL );
+	//
+	virtual RS_Vector getNearestCenter(const RS_Vector& coord,
+		double* dist  = NULL );
+	virtual RS_Vector getNearestMiddle(const RS_Vector& coord,
+		double* dist  = NULL);
+	virtual RS_Vector getNearestDist(double distance, 
+		const RS_Vector& coord, double* dist  = NULL);
+
+	virtual void addControlPoint(const RS_Vector& v);
+	virtual void removeLastControlPoint();
+
+	virtual void move(RS_Vector offset);
+	virtual void rotate(RS_Vector center, double angle);
+	virtual void scale(RS_Vector center, RS_Vector factor);
+	virtual void mirror(RS_Vector axisPoint1, RS_Vector axisPoint2);
+	virtual void moveRef(const RS_Vector& ref, const RS_Vector& offset);
+
+	virtual void draw(RS_Painter* painter, RS_GraphicView* view, double patternOffset = 0.0);
+	RS_ValueList<RS_Vector> getControlPoints();
+
+	friend std::ostream& operator << (std::ostream& os, const RS_Spline& l);
+
+	virtual void calculateBorders();
+
+	static void rbasis(int c, double t, int npts, int x[], double h[], double r[]);
+
+	static void knot(int num, int order, int knotVector[]);
+	static void rbsplinu(int npts, int k, int p1,
+		double b[], double h[], double p[]);;
+
+protected:
+	RS_SplineData data;
 };
+
+#endif 
